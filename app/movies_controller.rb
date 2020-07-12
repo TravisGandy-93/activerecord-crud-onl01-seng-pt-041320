a1d1bef3e15a1e24881bcc7f89548b527b3e453c
Travis Gandy <travis_gandy@yahoo.com>
2020-07-12 17:59:44 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,11 +24,8 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block
- 
-  Movie.create{|m|
-    m.title = 'Home Alone'
-    m.release_date = 1990
-  }
+    
+
 end
 
 def can_get_the_first_item_in_the_database
